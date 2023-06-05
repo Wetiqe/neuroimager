03bbf22e298b91817a3ccaea3b05ae5702e05180
Wetiqe <jzni132134@gmail.com>
2023-06-05 18:22:56 +0800

    move to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='neuroimager',
-    version='0.0.1',
+    version='0.0.2',
     description='A collection of utilities used for MRI data analysis',
     author='Wetiqe@GitHub',
     author_email='jzni132134@gmail.com',
