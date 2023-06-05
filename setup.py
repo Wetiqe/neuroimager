67cd9db635a01814269d5f6d9a372b1cebc566ac
Wetiqe <jzni132134@gmail.com>
2023-06-05 17:33:51 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='neuroimager',
+    version='0.0.1',
+    description='A collection of utilities used for MRI data analysis',
+    author='Wetiqe@GitHub',
+    author_email='jzni132134@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'scipy',
+        'sklearn',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
+
