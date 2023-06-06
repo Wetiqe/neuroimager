8de19fecf84825729a2bba2e86150c3a183cec41
Wetiqe <jzni132134@gmail.com>
2023-06-06 15:51:18 +0800

    Add partial correlation

--- neuroimager/plotting/plot.py
+++ neuroimager/plotting/plot.py
@@ -4,6 +4,37 @@ import seaborn as sns
 from scipy.stats import gaussian_kde
 
 
+def update_matplotlib_fontsize():
+    parameters = {'axes.labelsize': 20,
+                 'axes.titlesize': 20,
+                 'figure.titlesize':20,
+                 'xtick.labelsize':20,
+                 'ytick.labelsize':20,
+                 'legend.fontsize':15,
+                 'legend.title_fontsize':16,
+                 'axes.titleweight':'bold',
+                 'axes.labelweight': 'bold',
+                 'figure.titleweight':'bold',
+                 'font.weight':'bold',
+                 'font.sans-serif':'Arial'}
+    plt.rcParams.update(parameters)
+        
+
+def get_sig(df,feature):
+    sig = df.loc[scale,'p value']
+    symbol = ''
+    if  0.01 < sig < 0.05:
+        symbol = '*'
+    elif 0.001< sig <=0.01:
+        symbol = '**'
+    elif sig <=0.001:
+        symbol = '***'
+    elif 0.05 <= sig <0.10:
+        symbol = '^'
+    return (sig, symbol)
+    
+
+
 def density_scatter(
     x, y, reg=True, gaussian_density=True, figsize=(10, 10), display=True, **kwargs
 ):
@@ -31,3 +62,95 @@ def density_scatter(
         plt.show()
 
     return fig, ax
+    
+def plot_time_series(
+    time_series,
+    n_samples=None,
+    y_tick_values=None,
+    plot_kwargs=None,
+    fig_kwargs=None,
+    ax=None,
+    filename=None,
+):
+    """Plot a time series with channel separation.
+    ***This is a function modified from else where. Add the source*** 
+    Parameters
+    ----------
+    time_series : numpy.ndarray
+        The time series to be plotted. Shape must be (n_samples, n_channels).
+    n_samples : int
+        The number of time points to be plotted.
+    y_tick_values:
+        Labels for the channels to be placed on the y-axis.
+    fig_kwargs : dict
+        Keyword arguments to be passed on to matplotlib.pyplot.subplots.
+    plot_kwargs : dict
+        Keyword arguments to be passed on to matplotlib.pyplot.plot.
+    ax : matplotlib.axes.Axes
+        The axis on which to plot the data. If not given, a new axis is created.
+    filename : str
+        Output filename.
+    Returns
+    -------
+    fig : matplotlib.pyplot.figure
+        Matplotlib figure object.
+    ax : matplotlib.pyplot.axis.
+        Matplotlib axis object(s).
+    """
+    time_series = np.asarray(time_series)
+    n_samples = min(n_samples or np.inf, time_series.shape[0])
+    n_channels = time_series.shape[1]
+
+    # Validation
+    if ax is not None:
+        if filename is not None:
+            raise ValueError(
+                "Please use plotting.save() to save the figure instead of the "
+                + "filename argument."
+            )
+        if isinstance(ax, np.ndarray):
+            raise ValueError("Only pass one axis.")
+
+    default_fig_kwargs = {"figsize": (12, 8)}
+    if fig_kwargs is None:
+        fig_kwargs = default_fig_kwargs
+    else:
+        fig_kwargs = override_dict_defaults(default_fig_kwargs, fig_kwargs)
+
+    default_plot_kwargs = {"lw": 0.7, "color": "tab:blue"}
+    if plot_kwargs is None:
+        plot_kwargs = default_plot_kwargs
+    else:
+        plot_kwargs = override_dict_defaults(default_plot_kwargs, plot_kwargs)
+
+    # Calculate separation
+    separation = (
+        np.maximum(time_series[:n_samples].max(), time_series[:n_samples].min()) * 1.2
+    )
+    gaps = np.arange(n_channels)[::-1] * separation
+
+    # Create figure
+    if ax is None:
+        fig, ax = create_figure(**fig_kwargs)
+
+    # Plot data
+    ax.plot(time_series[:n_samples] + gaps[None, :], **plot_kwargs)
+
+    ax.autoscale(tight=True)
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+
+    # Set x and y axis tick labels
+    ax.set_xticks([])
+    if y_tick_values is not None:
+        ax.set_yticks(gaps)
+        ax.set_yticklabels(y_tick_values)
+    else:
+        ax.set_yticks([])
+
+    # Save figure
+    if filename is not None:
+        save(fig, filename)
+
+    return fig, 
+
---
+++ neuroimager/stats/correlation.py
@@ -0,0 +1,133 @@
+import numpy as np
+import pingouin as pg
+import pandas as pd
+
+
+def partial_corrs(
+    data,
+    x_labels: list = None,
+    y_labels: list = None,
+    covariates: list = None,
+    method="auto",
+    semi=False,
+    alternative="two-sided",
+    correct_p="global",
+    correct_method="holm",
+):
+    """
+    Compute partial correlations between pairs of variables, controlling for specified covariates.
+
+    Parameters
+    ----------
+    data : pd.DataFrame or np.ndarray
+        A 2D Pandas DataFrame or NumPy Array containing the data.
+    x_labels : list
+        A list of column indices or names for the independent variables.
+    y_labels : list
+        A list of column indices or names for the dependent variables.
+    covariates : list, optional
+        A list of column indices or names for the covariates to control for. Default is None.
+    method : str, optional
+        The correlation method to use. Options are 'auto', 'pearson', or 'spearman'. Default is 'auto'.
+    semi : bool or str, optional
+        If True, compute semi-partial correlation. If 'x', control for covariates only in x. If 'y', control for covariates only in y. Default is False.
+    alternative : str, optional
+        The alternative hypothesis for the correlation test. Options are 'two-sided', 'greater', or 'less'. Default is 'two-sided'.
+    correct_p : str, optional
+        The correction method for multiple comparisons. Options are 'global', 'IVs', or any other value for no correction. Default is 'global'.
+    correct_method : str, optional
+        The method to use for multiple comparison correction. Options are 'holm', 'bonferroni', 'fdr_bh', 'fdr_by', 'fdr_tsbh', 'fdr_tsbky', 'sidak', or 'none'. Default is 'holm'.
+
+    Returns
+    -------
+    r_matrix : pd.DataFrame
+        A DataFrame containing the partial correlation coefficients between the independent and dependent variables.
+    p_matrix : pd.DataFrame
+        A DataFrame containing the p-values for the partial correlation coefficients.
+
+    Notes
+    -----
+    This function computes partial correlations between pairs of variables, controlling for specified covariates.
+    P-values are corrected for multiple comparisons using the specified method. 
+    The function supports both Pearson and Spearman correlation methods, as well as semi-partial correlations.
+
+    References
+    ----------
+    1. Pingouin documentation: https://pingouin-stats.org/generated/pingouin.partial_corr.html
+    2. Pingouin multiple comparison correction: https://pingouin-stats.org/generated/pingouin.multicomp.html
+    """
+    if not isinstance(data, pd.DataFrame):
+        try:
+            data = pd.DataFrame(data)
+        except:
+            raise ValueError("data must be a 2D Pandas DataFrame or NumPy Array")
+    if len(x_labels) == 0 or len(y_labels) == 0:
+        raise ValueError("X and Y must be specified")
+    if len(covars) == 0:
+        print(
+            "You did not specify any covariates, will proceed with simple corrlelation"
+        )
+    IVs = data.columns[x_labels]  # independent variables
+    DVs = data.columns[y_labels]  # dependent variables
+    r_matrix = pd.DataFrame(np.zeros((len(IVs), len(DVs))), index=IVs, columns=DVs)
+    p_matrix = r_matrix.copy()
+
+    for y_label in DVs:
+        p_vals = []
+        for x_label in IVs:
+            # Create a df only contains the variables using now
+            # This is a good practice when using pingouin
+            df = data[[x_label, y_label] + covariates]
+            df = df.dropna(how="any", axis=0)  # drop any row with missing value
+
+            # Select correlation method based on multivariate normality test result
+            if method == "auto":
+                method_to_use = (
+                    "pearson"
+                    if pg.multivariate_normality(df[[x_label, y_label]])[2]
+                    else "spearman"
+                )
+            else:
+                method_to_use = method
+            if not semi:
+                results = pg.partial_corr(
+                    data=df,
+                    x=x_label,
+                    y=y_label,
+                    covar=covariates,
+                    alternative=alternative,
+                    method=method_to_use,
+                ).round(3)
+            elif semi == "x":
+                results = pg.partial_corr(
+                    data=df,
+                    x=x_label,
+                    y=y_label,
+                    x_covar=covariates,
+                    alternative=alternative,
+                    method=method_to_use,
+                ).round(3)
+            elif semi == "y":
+                results = pg.partial_corr(
+                    data=df,
+                    x=x_label,
+                    y=y_label,
+                    y_covar=covariates,
+                    alternative=alternative,
+                    method=method_to_use,
+                ).round(3)
+
+            r_matrix.loc[x_label, y_label] = results.loc[method_to_use, "r"]
+            p_vals.append(results.loc[method_to_use, "p-val"])
+        if correct_p == "IVs":
+            bools, p_vals = pg.multicomp(p_vals, alpha=0.05, method=correct_method)
+        p_matrix.loc[:, y_label] = p_vals
+    if correct_p == "global":
+        pvals = p_matrix.values.flatten()
+        bools, p_vals = pg.multicomp(p_vals, alpha=0.05, method=correct_method)
+        p_matrix = pd.DataFrame(
+            p_vals.reshape(len(x_labels), len(y_labels)),
+            index=p_matrix.index,
+            columns=p_matrix.columns,
+        )
+    return r_matrix, p_matrix
