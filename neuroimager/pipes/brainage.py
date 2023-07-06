64388cdf18f51f661494346c37598e404851e1d9
Wetiqe <jzni132134@gmail.com>
2023-07-06 22:24:16 +0800

    Make brain age correction support sklearn estimator style

--- neuroimager/pipes/brainage.py
+++ neuroimager/pipes/brainage.py
@@ -1,6 +1,28 @@
 import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+
+
+class BrainAgeGapCorrectionLinear(BaseEstimator, TransformerMixin):
+    #     References:
+    #     Bias-adjustment in neuroimaging-based brain age frameworks: A robust scheme
+    def __init__(self, line_reg_order=1):
+        self.line_reg_order = line_reg_order
+        self.line_reg = LinearRegression()
+        self.poly = PolynomialFeatures(degree=self.line_reg_order, include_bias=True)
+
+    def fit(self, X, y):
+        line_reg_features = self.poly.fit_transform(y.reshape(-1, 1))
+        offset_train = X - y
+        self.line_reg.fit(line_reg_features, offset_train.reshape(-1, 1))
+        return self
+
+    def transform(self, X, y):
+        test_features = self.poly.transform(y.reshape(-1, 1))
+        offset_test = np.array(self.line_reg.predict(test_features)).flatten()
+        corrected_age = X - offset_test
+        return corrected_age.reshape(-1, 1)
 
 
 def BAG(train_pred, test_pred, train_y, test_y, line_reg_order=1):
@@ -18,8 +40,6 @@ def BAG(train_pred, test_pred, train_y, test_y, line_reg_order=1):
     train_delta_age (numpy array): Difference between predicted and actual age on the training set.
     line_reg (LinearRegression object): Trained regression model.
 
-    References:
-    Bias-adjustment in neuroimaging-based brain age frameworks: A robust scheme
     """
 
     line_reg = LinearRegression()
